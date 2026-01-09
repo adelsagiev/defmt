8cbe99d6633bc6ca3635edde275332b4ec058a4c
doryan <sagiev2015@yandex.ru>
2026-01-09 19:01:24 +0400

    change avr target

--- defmt/build.rs
+++ defmt/build.rs
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // `"atomic-cas": false` in `--print target-spec-json`
     // last updated: rust 1.48.0
     match &target[..] {
-        "avr-gnu-base"
+        "avr-none"
         | "msp430-none-elf"
         | "riscv32i-unknown-none-elf"
         | "riscv32imc-unknown-none-elf"
